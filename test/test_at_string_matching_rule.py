c46b9fc26f74a3134b5bada2eca070d1a1b09e3d
Mohammad Mohsen <kuro.ece@gmail.com>
2021-09-29 17:36:54 +0200

    Changes: - test_at_string_matching_rule.py:   - Added test cases for AtStringMatchingRule

--- test/test_at_string_matching_rule.py
+++ test/test_at_string_matching_rule.py
@@ -3,36 +3,68 @@
 
 
 from unittest import TestCase
-import serial
-from at_cmd_client import AtStringMatchingRule, AtCommandClient
+
+from at_cmd_client import AtString, AtStringMatchingRule
 
 
 class Test(TestCase):
 
-    def setUp(self) -> None:
-        self.client = AtCommandClient(
-            name=__name__,
-            uart_handle=serial.Serial()
+    def test_at_string_matching_rule_exact_true(self):
+        self.assertEqual(
+            AtString.match_string("OK", "OK", AtStringMatchingRule.Exact),
+            "OK",
         )
 
-    def test_at_string_matching_rule_exact_true(self):
-        self.assertTrue(self.client.match_string("OK", "OK", AtStringMatchingRule.Exact))
-
     def test_at_string_matching_rule_exact_at_end(self):
-        self.assertTrue(self.client.match_string("OK", "Before OK\r\n", AtStringMatchingRule.Exact))
+        self.assertEqual(
+            AtString.match_string("OK", "Before OK", AtStringMatchingRule.Exact),
+            "OK",
+        )
 
     def test_at_string_matching_rule_exact_at_start(self):
-        self.assertTrue(self.client.match_string("OK", "OK After\r\n", AtStringMatchingRule.Exact))
+        self.assertEqual(
+            AtString.match_string("OK", "OK After", AtStringMatchingRule.Exact),
+            "OK",
+        )
 
     def test_at_string_matching_rule_exact_at_substring(self):
-        self.assertTrue(self.client.match_string("OK", "Before OK After\r\n", AtStringMatchingRule.Exact))
+        self.assertEqual(
+            AtString.match_string("OK", "Before OK After", AtStringMatchingRule.Exact),
+            "OK",
+        )
 
     def test_at_string_matching_rule_exact_newline(self):
-        self.assertTrue(self.client.match_string("OK", "OK\r\n", AtStringMatchingRule.Exact))
+        self.assertEqual(
+            AtString.match_string("OK", "OK\r\n", AtStringMatchingRule.Exact),
+            "OK",
+        )
 
     def test_at_string_matching_rule_exact_different_case(self):
-        self.assertFalse(self.client.match_string("ok", "OK\r\n", AtStringMatchingRule.Exact))
+        self.assertEqual(
+            AtString.match_string("OK", "ok", AtStringMatchingRule.Exact),
+            "",
+        )
 
+    def test_at_string_matching_rule_regex_string(self):
+        self.assertEqual(
+            AtString.match_string("OK", "OK", AtStringMatchingRule.Regex),
+            "OK",
+        )
 
-if __name__ == '__main__':
-    pass
+    def test_at_string_matching_rule_regex_pattern_true(self):
+        self.assertEqual(
+            AtString.match_string("\\w{2}", "OK", AtStringMatchingRule.Regex),
+            "OK",
+        )
+
+    def test_at_string_matching_rule_regex_pattern_false(self):
+        self.assertEqual(
+            AtString.match_string("[a-zA-Z]{2}", "12", AtStringMatchingRule.Regex),
+            "",
+        )
+
+    def test_at_string_matching_rule_regex_value_pattern(self):
+        self.assertEqual(
+            AtString.match_string("\\+CME ERROR:\\s*\\d*", "+CME ERROR: 53", AtStringMatchingRule.Regex),
+            "+CME ERROR: 53",
+        )
